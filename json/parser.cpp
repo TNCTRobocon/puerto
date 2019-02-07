47b14304b29897465bd2c204cd812a53bd59238d
terurin <hamusutaateru@live.jp>
2019-02-07 14:30:55 +0900

    rewirte for adapting region interface

--- json/parser.cpp
+++ json/parser.cpp
@@ -1,72 +1,84 @@
 #include "parser.hpp"
+#include <algorithm>
 using namespace std;
-namespace BindJson {
+namespace BindJson ::Parser {
 
-Iterator ParserAny::operator()(Iterator begin, Iterator end) const {
-    for (auto now = begin;;) {
-        if (now == end) {
-            return now;
-        } else if (auto next = (*child)(now, end); next != now) {
-            now = next;
-        } else {
-            return now;
-        }
+wstring Region::ToString() const {
+    wstring result;
+    copy(begin, end, back_inserter(result));
+    return result;
+}
+
+Region CharactorList::operator()(const Region &now) const {
+    if (wchar_t cmp = *now;
+        now.IsActive() && any_of(list.begin(), list.end(),
+                                 [cmp](wchar_t c) { return cmp == c; })) {
+        return now + 1;
+    } else {
+        return now;
     }
 }
 
-Iterator ParserSome::operator()(Iterator begin, Iterator end) const {
-    // n回回す
-    auto now = begin;
-    for (unsigned int cnt = 0; cnt < n; cnt++) {
-        if (now == end) {
-            return begin;
-        } else if (auto next = (*child)(now, end); next != now) {
-            now = next;
-        } else {
-            return begin;
-        }
-    }
-    //回せるだけ回す
-    for (;;) {
-        if (now == end) {
-            return now;
-        } else if (auto next = (*child)(now, end); next != now) {
-            now = next;
-        } else {
-            return now;
-        }
+Region ParserOr::operator()(const Region &now) const {
+    if (!now) {
+        return now;
+    } else if (auto next = (*a)(now); now != next) {
+        return next;
+    } else if (auto next = (*b)(now); now != next) {
+        return next;
+    } else {
+        return now;
     }
 }
 
-Iterator ParserMul::operator()(Iterator begin, Iterator end) const {
-    // n回回す
-    auto now = begin;
-    for (unsigned int cnt = 0; cnt < n; cnt++) {
-        if (now == end) {
-            return begin;
-        } else if (auto next = (*child)(now, end); next != now) {
-            now = next;
-        } else {
-            return begin;
+Region ParserAnd::operator()(const Region &now) const {
+    if (!now) {
+        return now;
+    }
+    if (auto next = (*a)(now); next != now) {
+        if (auto next2 = (*b)(next); next2 != next) {
+            return next2;
         }
     }
     return now;
 }
 
-ParserPointer operator|(ParserPointer a, ParserPointer b) {
-    return make_unique<ParserOr>(move(a), move(b));
+Region ParserAny::operator()(const Region &last) const {
+    if (!last) {
+        return last;
+    }
+    auto now = last, next = last;
+    // N回試行
+    for (unsigned int count = 0; count < n; count++) {
+        next = (*child)(now);
+        if (now == next) {
+            return last;
+        }
+        now = next;
+    }
+    while (true) {
+        next = (*child)(now);
+        if (now == next) {
+            return now;
+        }
+        now = next;
+    }
 }
 
-ParserPointer operator&(ParserPointer a, ParserPointer b) {
-    return make_unique<ParserAnd>(move(a), move(b));
+Region ParserSome::operator()(const Region &last) const {
+    if (!last) {
+        return last;
+    }
+    auto now = last, next = last;
+    // N回試行
+    for (unsigned int count = 0; count < n; count++) {
+        next = (*child)(now);
+        if (now == next) {
+            return last;
+        }
+        now = next;
+    }
+    return now;
 }
 
-ParserPointer operator+(ParserPointer a, unsigned int n) {
-    return make_unique<ParserSome>(move(a), n);
-}
-
-ParserPointer operator*(ParserPointer a, unsigned int n) {
-    return make_unique<ParserMul>(move(a), n);
-}
-
-}  // namespace BindJson
+}  // namespace BindJson::Parser
--- json/parser.hpp
+++ json/parser.hpp
@@ -3,130 +3,201 @@
 #define __PARSER_HEADER_GUARD__
 
 #include <stddef.h>
+#include <wchar.h>
 #include <algorithm>
 #include <cctype>
 #include <functional>
 #include <memory>
 #include <string>
 
-namespace BindJson {
-using Iterator = std::string::const_iterator;
+namespace BindJson ::Parser {
+
+struct Region final {
+    using iterator = std::wstring::const_iterator;
+    iterator begin, end;
+    Region(const std::wstring &text) : begin(text.cbegin()), end(text.cend()) {}
+    Region(iterator _begin, iterator _end) : begin(_begin), end(_end) {}
+    Region(const Region &orign) = default;
+    ~Region() = default;
+    //演算子
+    Region operator=(const Region &orign) {
+        return Region(begin = orign.begin, end = orign.end);
+    }
+    bool operator==(const Region &cmp) const {
+        return begin == cmp.begin && end == cmp.end;
+    }
+    bool operator!=(const Region &cmp) const {
+        return begin != cmp.begin || end != cmp.end;
+    }
+    bool operator!() const {
+        return begin >= end;
+    }
+    // iterator
+    wchar_t operator*() const {
+        return *begin;
+    }
+    Region operator+(int pos) const {
+        return Region(begin + pos, end);
+    }
+    Region operator+=(int pos) {
+        return Region(begin += pos, end);
+    }
+    Region operator++() {
+        return Region(++begin, end);
+    }
+    Region operator++(int) {
+        return Region(begin++, end);
+    }
+    Region operator-(int pos) const {
+        return Region(begin + pos, end);
+    }
+    Region operator-=(int pos) {
+        return Region(begin -= pos, end);
+    }
+    Region operator--() {
+        return Region(--begin, end);
+    }  // iterator
+    Region operator--(int) {
+        return Region(begin--, end);
+    }
+    //プロパティ
+    std::ptrdiff_t Distance() const {
+        return std::distance(begin, end);
+    }
+    std::wstring ToString() const;
+    //有効かどうかの確認
+    bool IsActive() const {
+        return begin < end;
+    }
+};
+
 class ParserBase {
 public:
-    virtual Iterator operator()(Iterator now, Iterator end) const = 0;
+    ParserBase() = default;
+    ParserBase(const ParserBase &) = default;
+    virtual ~ParserBase() = default;
+    virtual Region operator()(const Region &now) const = 0;
 };
 
-using ParserPointer = std::unique_ptr<ParserBase>;
+using ParserPointer = std::shared_ptr<ParserBase>;
 
-class ParserDigit : public ParserBase {
+class CharactorItem : public ParserBase {
+    const wchar_t value;
+
 public:
-    virtual Iterator operator()(Iterator it, Iterator end) const {
-        return (it != end) && isdigit(*it) ? it + 1 : it;
+    CharactorItem(wchar_t _value) : value(_value) {}
+    CharactorItem(const CharactorItem &) = default;
+    virtual ~CharactorItem() = default;
+    virtual Region operator()(const Region &reg) const {
+        return (reg.IsActive() && (*reg == value)) ? reg + 1 : reg;
     }
 };
 
-class ParserCharactorList : public ParserBase {
-    const std::string match;
+inline ParserPointer Item(wchar_t c) {
+    return ParserPointer(std::make_shared<CharactorItem>(c));
+}
+
+class CharactorList : public ParserBase {
+    const std::wstring list;
+
 public:
-    ParserCharactorList(const std::string& _match) : match(_match) {}
-    virtual Iterator operator()(Iterator it, Iterator end) const {
-        return (it != end) &&
-                       std::any_of(match.begin(), match.end(),
-                                   [it](char c) -> bool { return *it == c; })
-                   ? it + 1
-                   : it;
+    CharactorList(const std::wstring &_list) : list(_list) {}
+    CharactorList(const CharactorList &) = default;
+    virtual ~CharactorList() = default;
+    virtual Region operator()(const Region &reg) const;
+};
+
+inline ParserPointer List(const std::wstring &list) {
+    return ParserPointer(std::make_shared<CharactorList>(list));
+}
+
+class CharactorRange : public ParserBase {
+    const wchar_t begin, end;
+
+public:
+    CharactorRange(wchar_t _begin, wchar_t _end) : begin(_begin), end(_end) {}
+    CharactorRange(const CharactorRange &) = default;
+    virtual ~CharactorRange() = default;
+    virtual Region operator()(const Region &reg) const {
+        return reg.IsActive() && (begin <= *reg) && (*reg <= end) ? reg + 1
+                                                                  : reg;
     }
 };
 
-class ParserCharactor : public ParserBase {
-    const char c;
+inline ParserPointer Range(wchar_t begin, wchar_t end) {
+    return ParserPointer(std::make_shared<CharactorRange>(begin, end));
+}
 
+class ParserSkip : public ParserBase {
+    const unsigned int n;  // n回
 public:
-    ParserCharactor(char _c) : c(_c) {}
-    virtual Iterator operator()(Iterator it, Iterator end) const {
-        return (it != end) && *it == c ? it + 1 : it;
+    ParserSkip(unsigned int _n) : n(_n) {}
+    ParserSkip(const ParserSkip &) = default;
+    virtual ~ParserSkip() = default;
+    virtual Region operator()(const Region &now) const {
+        if (auto next = now + n; next.IsActive()) {
+            return next;
+        } else {
+            return now;
+        }
     }
-};
-
-class ParserRange : public ParserBase {
-    const char start,finish;
-public:
-    ParserRange(char _start,char _finish) : start(_start),finish(_finish){}
-    virtual Iterator operator()(Iterator it, Iterator end) const {
-        return (it != end) && (start<=*it)&&(*it<finish)? it + 1 : it;
-    }
-};
-
-// 0回以上
-class ParserAny : public ParserBase {
-    const std::unique_ptr<ParserBase> child;
-
-public:
-    ParserAny(std::unique_ptr<ParserBase> _child) : child(std::move(_child)) {}
-    virtual Iterator operator()(Iterator begin, Iterator end) const;
-};
-
-// n回以上
-class ParserSome : public ParserBase {
-    const ParserPointer child;
-    const unsigned int n;
-
-public:
-    ParserSome(ParserPointer _child, unsigned int _n = 1)
-        : child(std::move(_child)), n(_n) {}
-    virtual Iterator operator()(Iterator begin, Iterator end) const;
-};
-
-// n回
-class ParserMul : public ParserBase {
-    const ParserPointer child;
-    const unsigned int n;
-
-public:
-    ParserMul(ParserPointer _child, unsigned int _n = 1)
-        : child(std::move(_child)), n(_n) {}
-    virtual Iterator operator()(Iterator begin, Iterator end) const;
 };
 
 class ParserOr : public ParserBase {
-    ParserPointer a, b;
+    const ParserPointer a, b;
 
 public:
-    ParserOr(ParserPointer _a, ParserPointer _b)
-        : a(std::move(_a)), b(std::move(_b)) {}
-    virtual Iterator operator()(Iterator begin, Iterator end) const {
-        if (auto now = (*a)(begin, end); now != begin) {
-            return now;
-        } else if (auto now = (*b)(begin, end); now != begin) {
-            return now;
-        } else {
-            return begin;
-        }
-    }
+    ParserOr(ParserPointer _a, ParserPointer _b) : a(_a), b(_b) {}
+    ParserOr(const ParserOr &) = default;
+    virtual ~ParserOr() = default;
+    virtual Region operator()(const Region &reg) const;
 };
+inline ParserPointer operator||(ParserPointer a, ParserPointer b) {
+    return ParserPointer(std::make_shared<ParserOr>(a, b));
+}
 
 class ParserAnd : public ParserBase {
-    ParserPointer a, b;
+    const ParserPointer a, b;
 
 public:
-    ParserAnd(ParserPointer _a, ParserPointer _b)
-        : a(std::move(_a)), b(std::move(_b)) {}
-    virtual Iterator operator()(Iterator begin, Iterator end) const {
-        if (auto now = (*a)(begin, end); now != begin) {
-            if (auto next = (*b)(now, end); next != now) {
-                return next;
-            }
-        }
-        return begin;
-    }
+    ParserAnd(ParserPointer _a, ParserPointer _b) : a(_a), b(_b) {}
+    ParserAnd(const ParserAnd &) = default;
+    virtual ~ParserAnd() = default;
+    virtual Region operator()(const Region &reg) const;
 };
 
-ParserPointer operator|(ParserPointer a, ParserPointer b);
-ParserPointer operator&(ParserPointer a, ParserPointer b);
-ParserPointer operator+(ParserPointer a, unsigned int n);//n回以上
-ParserPointer operator*(ParserPointer a, unsigned int n);//n回
+inline ParserPointer operator&&(ParserPointer a, ParserPointer b) {
+    return ParserPointer(std::make_shared<ParserAnd>(a, b));
+}
 
+class ParserAny : public ParserBase {
+    const ParserPointer child;
+    const unsigned int n;  // n回以上
+public:
+    ParserAny(ParserPointer _child, unsigned int _n) : child(_child), n(_n) {}
+    ParserAny(const ParserAny &) = default;
+    virtual ~ParserAny() = default;
+    virtual Region operator()(const Region &now) const;
+};
 
-}  // namespace BindJson
+inline ParserPointer operator+(ParserPointer a, unsigned int n) {
+    return ParserPointer(std::make_shared<ParserAny>(a, n));
+}
+
+class ParserSome : public ParserBase {
+    const ParserPointer child;
+    const unsigned int n;  // n回
+public:
+    ParserSome(ParserPointer _child, unsigned int _n) : child(_child), n(_n) {}
+    ParserSome(const ParserSome &) = default;
+    virtual ~ParserSome() = default;
+    virtual Region operator()(const Region &now) const;
+};
+
+inline ParserPointer operator*(ParserPointer a, unsigned int n) {
+    return ParserPointer(std::make_shared<ParserSome>(a, n));
+}
+
+}  // namespace BindJson::Parser
 
 #endif
