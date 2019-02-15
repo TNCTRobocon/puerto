10cf40d301db0c204c48b708d5cfdf562091c2d4
terurin <hamusutaateru@live.jp>
2019-02-16 02:57:10 +0900

    同期風に書いてみた

--- serial/port.cpp
+++ serial/port.cpp
@@ -1,5 +1,5 @@
 #include "port.hpp"
-
+#include <algorithm>
 namespace Serial {
 using namespace std;
 using namespace boost::asio;
@@ -43,6 +43,8 @@ bool Session::Clear() {
     return false;
 }
 
+constexpr char SerialPort::newline;
+
 SerialPort::SerialPort(const std::string& path, int baud)
     : port(service, path) {
     // serial portを初期化する
@@ -53,6 +55,62 @@ SerialPort::SerialPort(const std::string& path, int baud)
     port.set_option(serial_port_base::parity(serial_port_base::parity::none));
     port.set_option(
         serial_port_base::stop_bits(serial_port_base::stop_bits::one));
+}
+
+bool SerialPort::Add(std::shared_ptr<Session> session) {
+    unique_lock<mutex> block(lock, defer_lock);
+    if (block.try_lock()) {
+        list.emplace_back(session);
+        return true;
+    }
+    return false;
+}
+
+bool SerialPort::Remove(std::shared_ptr<Session> target) {
+    unique_lock<mutex> block(lock, defer_lock);
+    if (block.try_lock()) {
+        list.erase(remove(list.begin(), list.end(), target), list.end());
+        return true;
+    }
+    return false;
+}
+
+bool SerialPort::Process() {
+    unique_lock<mutex> block(lock, defer_lock);
+    if (block.try_lock()) {
+        for (shared_ptr<Session> session : list) {
+            // Open
+            unique_lock<mutex> block(session->lock);
+            uint8_t target = (session->address & 0x7f) | 0x80;
+            port.write_some(target);
+            // TODO あとで真面目に書く
+            // Write All
+            for (const auto& line : session->send) {
+                auto tmp = line + newline;
+                port.write_some(buffer(tmp));
+            }
+            // Read All Until 0x80
+            string line;
+            while (1) {
+                uint8_t c;
+                port.read_some(buffer(&c, 1));
+                switch (c) {
+                    case 0x80:
+                        break;
+                    case '\r':
+                        session->receive.emplace_back(line);
+                        line = "";
+                        break;
+                    default:
+                        line += c;
+                }
+            }
+        }
+        // Close
+        port.write_some(0x80);
+        return true;
+    }
+    return false;
 }
 
 }  // namespace Serial
--- serial/port.hpp
+++ serial/port.hpp
@@ -2,43 +2,51 @@
 #ifndef __PORT_HEADER_GUARD__
 #define __PORT_HEADER_GUARD__
 #include <boost/asio.hpp>
+#include <deque>
 #include <memory>
 #include <mutex>
 #include <string>
-#include <deque>
 namespace Serial {
 
 //仮想的に1対1の接続を行うクラス
-class Session final{
+class SerialPort;
+class Session final {
 public:
+    friend class SerialPort;
     const int address;
+
 private:
-    mutable std::mutex  lock;
+    mutable std::mutex lock;
     std::deque<std::string> send;
     std::deque<std::string> receive;
-   
+
 public:
     Session(int _address) : address(_address) {}
-    Session(const Session&)=delete;
-    ~Session()=default;
-    bool Send(const std::string& line);//失敗したらfalse
-    bool Receive(std::string& line);//失敗したらfalse
-    bool IsConnected()const;//通信が終わっていたらtrue
-    bool Clear();//消すことができたらtrue
+    Session(const Session&) = delete;
+    ~Session() = default;
+    bool Send(const std::string& line);  //失敗したらfalse
+    bool Receive(std::string& line);     //失敗したらfalse
+    bool IsConnected() const;            //通信が終わっていたらtrue
+    bool Clear();                        //消すことができたらtrue
 };
 
-class Session;
 class SerialPort final {
+public:
+    constexpr static char newline{'\r'};
+private:
     boost::asio::io_service service;
     boost::asio::serial_port port;
     std::vector<std::shared_ptr<Session>> list;
+    std::mutex lock;
 
 public:
     SerialPort(const std::string& path, int baud = B115200);
     SerialPort() = delete;
     ~SerialPort() = default;
+    bool Add(std::shared_ptr<Session> session);
+    bool Remove(std::shared_ptr<Session> target);
+    bool Process();
 };
-
 
 }  // namespace Serial
 #endif
