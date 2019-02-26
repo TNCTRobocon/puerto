eefcea55d24fb834f6ab03cff4f912378da8ab51
terurin <hamusutaateru@live.jp>
2019-02-27 00:58:43 +0900

    mutableを無効化

--- boot/flags.hpp
+++ boot/flags.hpp
@@ -15,25 +15,27 @@ public:
     FlagBase(const FlagBase&) = default;
     virtual ~FlagBase() = default;
     virtual void Set(const std::string&) {}
-    virtual std::string GetUsage() const { return usage; }
+    virtual std::string GetUsage() const {
+        return usage;
+    }
 };
 
 class FlagString : public FlagBase {
     std::string& value;
+
 public:
-    FlagString(std::string& _value, const std::string& _usage)
-        : FlagBase(_usage), value(_value) {}
-    virtual void Set(const std::string& inst) { value = inst; }
+    FlagString(std::string& _value, const std::string& _usage) : FlagBase(_usage), value(_value) {}
+    virtual void Set(const std::string& inst) {
+        value = inst;
+    }
 };
 
 class FlagInt : public FlagBase {
     int& value;
 
 public:
-    
-FlagInt(int& _value, const std::string& _usage)
-        : FlagBase(_usage), value(_value) {}
-    virtual void Set(const std::string& inst){
+    FlagInt(int& _value, const std::string& _usage) : FlagBase(_usage), value(_value) {}
+    virtual void Set(const std::string& inst) {
         value = std::stoi(inst);
     }
 };
@@ -42,40 +44,33 @@ class FlagBool : public FlagBase {
     bool& value;
 
 public:
-    FlagBool(bool& _value, const std::string& _usage)
-        : FlagBase(_usage), value(_value) {}
+    FlagBool(bool& _value, const std::string& _usage) : FlagBase(_usage), value(_value) {}
     virtual void Set(const std::string& inst);
-    void Set(bool x){
-        value=x;
+    void Set(bool x) {
+        value = x;
     }
 };
 
 class Flags {
-    mutable std::unordered_map<std::string, FlagBase*> flags;
+    std::unordered_map<std::string, FlagBase*> flags;
 
 public:
     Flags() = default;
     Flags(const Flags&) = delete;
     virtual ~Flags();
     // Add
-    void Add(const std::string& key,
-             std::string& value,
-             const std::string& usage) {
+    void Add(const std::string& key, std::string& value, const std::string& usage) {
         flags.emplace(key, new FlagString(value, usage));
     }
     void Add(const std::string& key, bool& value, const std::string& usage) {
         flags.emplace(key, new FlagBool(value, usage));
     }
     void Add(const std::string& key, int& value, const std::string& usage) {
-        flags.emplace(key, new 
-    FlagInt(value, usage));
+        flags.emplace(key, new FlagInt(value, usage));
     }
     // Parse
     void Parse(int argc, char** argv);
 };
-
-
-
 
 }  // namespace Boot
 #endif
