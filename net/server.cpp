de36793b97e19604c9a0ceb44d149978acd2d876
terurin <hamusutaateru@live.jp>
2019-03-05 07:40:27 +0900

    adapt poll

--- net/server.cpp
+++ net/server.cpp
@@ -19,21 +19,36 @@ Server::~Server() {
     zmq::message_t message;
     nearSocket.recv(&message);
     std::string response((char*)message.data(), message.size());
-    std::cout << message << std::endl;
+    std::cout << response << std::endl;
     thread.join();
 }
 
 void Server::Transfer() {
     zmq::socket_t nearSocket(context, zmq::socket_type::rep);
+    zmq::socket_t farSocket(context, zmq::socket_type::rep);
+
     nearSocket.bind(nearLocation);
-    zmq::message_t message;
+    farSocket.bind(farLocation);
+
+    zmq::pollitem_t items[2]{{nearSocket, 0, ZMQ_POLLIN, 0}, {farSocket, 0, ZMQ_POLLIN, 0}};
     while (1) {
-        nearSocket.recv(&message);
-        std::string request((char*)message.data(), message.size());
-        std::cout << request << std::endl;
-        nearSocket.send(request.data(), request.size());
-        if (request == "quit") return;
+        int n = zmq::poll(items, 2, -1);
+        std::cout << "test" << n << std::endl;
+        if (items[0].revents & ZMQ_POLLIN) {
+            zmq::message_t message;
+            nearSocket.recv(&message);
+            std::string request((char*)message.data(), message.size());
+            std::cout << request << std::endl;
+            nearSocket.send(request.data(), request.size());
+            if (request == "quit") return;
+        }
+        if (items[1].revents & ZMQ_POLLIN) {
+            zmq::message_t message;
+            farSocket.recv(&message);
+            std::string request((char*)message.data(), message.size());
+            std::cout << request << std::endl;
+            farSocket.send(request.data(), request.size());
+        }
     }
 }
-
 }  // namespace Net
--- net/server.hpp
+++ net/server.hpp
@@ -15,16 +15,17 @@
 namespace Net {
 
 class Server final {
-    zmq::context_t context;
-    const std::string nearLocation,farLocation;
+    zmq::context_t context;  //必ず先頭に置くこと
+    const std::string nearLocation, farLocation;
     std::thread thread;
-
 
 public:
     Server(const std::string& host = "*", int port = 40000);
     Server(const Server&) = delete;
     ~Server();
-    void Transfer();
+
+private:
+    void Transfer();//制約 thread以外から呼び出してはならない
 };
 
 }  // namespace Net
